c822afeaddb730e0d49beb50590c3a3fa415ff43
murilonicemento <murilo_nascimento22@outlook.com>
2023-01-20 16:39:37 -0300

    Completed project inclusion

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Projeto criado através da plataforma Rocketseat no evento NLW - Trilha Explorer
 
 ## Deploy da aplicação
-
+https://habit-control-beta.vercel.app/
 
 ## Tecnologias utilizadas
 + HTML5
@@ -10,7 +10,8 @@ Projeto criado através da plataforma Rocketseat no evento NLW - Trilha Explorer
 + JavaScript
 
 ## Estrutura do site
-
++ Botão: seção onde é possível registrar um novo dia
++ Checkbox: seção onde é possível ver os hábitos realizados em um determinado dia
 
 ## Home
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,28 @@
 const form = document.querySelector("form");
 const nlwSetup = new NLWSetup(form);
-const data = {
-    run: ["01-01", "01-02", "01-06"],
-    takePills: ["01-03"],
-    journal: ["01-02"]
+const button = document.querySelector("header button");
+
+button.addEventListener("click", add);
+form.addEventListener("change", save);
+
+function add() {
+    const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+    const dayExists = nlwSetup.dayExists(today);
+
+    if (dayExists) {
+        alert("Dia já existente");
+        return
+    }
+
+    nlwSetup.addDay(today);
+    alert("Dia adicionado com sucesso");
+
 }
 
+function save() {
+    localStorage.setItem("NlWSetup@habits", JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem("NlWSetup@habits")) || {};
 nlwSetup.setData(data);
 nlwSetup.load();
